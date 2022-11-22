22bc72467998850d3917f505808f9c773326fabc
romaniv2511 <romaniv2511@gmail.com>
2022-11-22 19:34:38 +0200

    Create Statistics.jsx

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,22 @@
+import PropTypes from 'prop-types';
+
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <ul>
+      <li>Good: {good}</li>
+      <li>Neutral: {neutral}</li>
+      <li>Bad: {bad}</li>
+      <li>Total: {total}</li>
+      <li>Positive percentage: {positivePercentage}%</li>
+    </ul>
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number,
+  neutral: PropTypes.number,
+  bad: PropTypes.number,
+  total: PropTypes.number,
+  positivePercentage: PropTypes.number,
+};
+export default Statistics;
